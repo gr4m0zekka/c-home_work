9285ea5126c8ba217aad9057788b4738427bc739
gr4m0zekka <101568879+gr4m0zekka@users.noreply.github.com>
2022-09-11 11:15:47 +0300

    Add files via upload

---
+++ fnd.c
@@ -0,0 +1,25 @@
+﻿
+
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <math.h>
+#include <iomanip>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+
+int main()
+{
+    int n;
+    scanf_s("%i", &n);
+    if (n % 2 == 0 && ((n >= 0) && (n <= 20) || (n > 100) && (n < 200))) {
+        printf("Yes");
+    }
+    else {
+        printf("No");
+    }
+}
+
