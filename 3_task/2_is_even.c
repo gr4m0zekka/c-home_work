f7b0d9e7eeeb170e6d661514abc7ab2952a4418e
gr4m0zekka <101568879+gr4m0zekka@users.noreply.github.com>
2022-09-20 23:26:26 +0300

    Add files via upload

---
+++ 3_task/2_is_even.c
@@ -0,0 +1,9 @@
+﻿#include <stdio.h>
+bool is_even(int n) {
+	return n % 2 == 0;
+}
+int main()
+{
+	printf("%i\n", is_even(90));
+	printf("%i\n", is_even(91));
+}
---
+++ 3_task/3_print_even.c
@@ -0,0 +1,16 @@
+﻿
+
+#include <iostream>
+void print_even(int a, int b) {
+    for (int i = a; i <= b; ++i) {
+        if (i % 2 == 0) {
+            printf("%i ", i);
+        }
+    }
+}
+int main()
+{
+    int a, b;
+    scanf_s("%i %i", &a, &b);
+    print_even(a, b);
+}
