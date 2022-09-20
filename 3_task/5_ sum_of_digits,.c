386e6c0c1f07fee5c2668082a72f3255d8a7c56a
gr4m0zekka <101568879+gr4m0zekka@users.noreply.github.com>
2022-09-20 23:40:38 +0300

    Add files via upload

---
+++ 3_task/5_ sum_of_digits,.c
@@ -0,0 +1,18 @@
+﻿
+
+#include <iostream>
+int  sum_of_digits(int n) {
+    int sum = 0;
+    while (n != 0) {
+        sum += n % 10;
+        n = (n - n % 10) / 10;
+    }
+    return sum;
+}
+int main()
+{
+    int n; 
+    scanf_s("%i", &n);
+    printf("%i", sum_of_digits(n));
+}
+
