93e254413bf7439608eee64777bd5948d96a1775
gr4m0zekka <101568879+gr4m0zekka@users.noreply.github.com>
2022-09-20 23:33:26 +0300

    Add files via upload

---
+++ 3_task/4_stars_triangle.c
@@ -0,0 +1,18 @@
+﻿
+
+#include <iostream>
+void triangle(int n) {
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 0; j < i; ++j) {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
+int main()
+{
+    int n; 
+    scanf_s("%i", &n);
+    triangle(n);
+}
+
