41d47a09152a5d2ab931822ecd2f353887668a28
gr4m0zekka <101568879+gr4m0zekka@users.noreply.github.com>
2022-09-21 23:28:08 +0300

    Add files via upload

---
+++ 3_task/7_tribogachi.c
@@ -0,0 +1,19 @@
+﻿
+
+#include <iostream>
+int trichi(int n) {
+    int mass[100];
+    mass[0] = mass[1] = 0;
+    mass[2] = 1;
+    for (int i = 3; i <= n; ++i) {
+        mass[i] = mass[i - 1] + mass[i - 2] + mass[i - 3];
+    }
+    return mass[n];
+}
+int main()
+{
+    int n;
+    scanf_s("%i", &n);
+    printf("%i", trichi(n));
+}
+
