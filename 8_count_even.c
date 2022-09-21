5bec89ab8505a28d702941084683cab07f49b288
gr4m0zekka <101568879+gr4m0zekka@users.noreply.github.com>
2022-09-21 23:47:48 +0300

    Add files via upload

---
+++ 8_count_even.c
@@ -0,0 +1,23 @@
+﻿
+
+#include <iostream>
+int count_even(int a[], int n) {
+    int count = 0;
+    for (int i = 0; i < n; ++i) {
+        if (a[i] % 2 == 0) {
+            count++;
+        }
+    }
+    return count;
+}
+int main()
+{
+    int n;
+    int a[100];
+    scanf_s("%i", &n);
+    for (int i = 0; i < n; ++i) {
+        scanf_s("%i", &a[i]);
+    }
+    printf("%i", count_even(a, n));
+}
+
