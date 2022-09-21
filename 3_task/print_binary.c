be9a34a7b01053c10ee746cc6a31986efb7a87c9
gr4m0zekka <101568879+gr4m0zekka@users.noreply.github.com>
2022-09-21 23:10:14 +0300

    Add files via upload

---
+++ 3_task/print_binary.c
@@ -0,0 +1,20 @@
+﻿
+
+#include <iostream>
+void print_binary(int n) {
+    int a;
+    if (n != 0) {
+        a = n % 2;
+        print_binary(n / 2);
+        printf("%i", a);
+    }
+ 
+
+}
+int main()
+{
+    int n;
+    scanf_s("%i", &n);
+    print_binary(n);
+}
+
