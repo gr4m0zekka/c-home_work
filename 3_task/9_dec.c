80f23ec8b3eb1ed453542381b12243086c3fa38a
gr4m0zekka <101568879+gr4m0zekka@users.noreply.github.com>
2022-09-22 00:28:45 +0300

    Add files via upload

---
+++ 3_task/9_dec.c
@@ -0,0 +1,23 @@
+﻿
+
+#include <iostream>
+void dec(int a[], int n) {
+    for (int i = 0; i < n; ++i) {
+        a[i] -= 1;
+    }
+}
+int main()
+    {
+        int n;
+        scanf_s("%i", &n);
+        int a[50];
+        for (int i = 0; i < n; ++i) {
+            scanf_s("%i", &a[i]);
+        }
+        dec(a, n);
+        for (int i = 0; i < n; ++i) {
+            printf("%i ", a[i]);
+        }
+    }
+
+
