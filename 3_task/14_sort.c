689903e6045c2aebb136512bc1c70d93c94f262b
gr4m0zekka <101568879+gr4m0zekka@users.noreply.github.com>
2022-09-22 23:09:22 +0300

    Add files via upload

---
+++ 3_task/14_sort.c
@@ -0,0 +1,28 @@
+﻿
+
+#include <iostream>
+void sort(int a[], int n) {
+    for (int i = 0; i < n; ++i) {
+        for (int i = 0; i < n; ++i) {
+            if (a[i] < a[i + 1]) {
+                int now = a[i];
+                a[i] = a[i + 1];
+                a[i + 1] = now;
+            }
+        }
+    }
+}
+int main()
+{
+    int n;
+    scanf_s("%i", &n);
+    int a[100];
+    for (int i = 0; i < n; ++i) {
+        scanf_s("%i", &a[i]);
+    }
+    sort(a, n);
+    for (int i = 0; i < n; ++i) {
+        printf("%i ", a[i]);
+    }
+}
+
