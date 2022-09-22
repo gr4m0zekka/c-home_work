c43860e7e4fcaa0bc35207825a88b3a5685e7de2
gr4m0zekka <101568879+gr4m0zekka@users.noreply.github.com>
2022-09-22 23:02:21 +0300

    Add files via upload

---
+++ 3_task/12_reverse.c
@@ -0,0 +1,24 @@
+﻿
+
+#include <iostream>
+void reverse(int a[], int n) {
+    for (int i = 0; i < n / 2; ++i) {
+        int now = a[i];
+        a[i] = a[n - 1 - i];
+        a[n - 1 - i] = now;
+    }
+}
+int main()
+{
+    int n;
+    scanf_s("%i", &n);
+    int a[100];
+    for (int i = 0; i < n; ++i) {
+        scanf_s("%i", &a[i]);
+    }
+    reverse(a, n);
+    for (int i = 0; i < n; ++i) {
+        printf("%i ", a[i]);
+    }
+}
+
---
+++ 3_task/13_reverse_subarray.c
@@ -0,0 +1,23 @@
+﻿
+
+#include <iostream>
+void reverse_subarray(int a[], int left, int right) {
+    for (int i = 0; i < (right - left) / 2; ++i) {
+        int now = a[left];
+        a[left + i] = a[right - 1 - i];
+        a[right - 1 - i] = now;
+    }
+}
+int main()
+{
+    int n, left, right;
+    scanf_s("%i %i %i", &n, &left, &right);
+    int a[100];
+    for (int i = 0; i < n; ++i) {
+        scanf_s("%i", &a[i]);
+    }
+    reverse_subarray(a, left, right);
+    for (int i = 0; i < n; ++i) {
+        printf("%i ", a[i]);
+    }
+}
