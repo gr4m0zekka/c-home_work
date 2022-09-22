63f8f4bf1dee4de74d4aceaae464c4731d2cf5c7
gr4m0zekka <101568879+gr4m0zekka@users.noreply.github.com>
2022-09-22 22:34:04 +0300

    Add files via upload

---
+++ 3_task/11_factorial.c
@@ -0,0 +1,34 @@
+﻿
+
+#include <iostream>
+int fact(int n) {
+    int fact = 1;
+    if (n) {
+        for (int i = 1; i <= n; ++i) {
+            fact *= i;
+        }
+    }
+    else {
+        return 1;
+    }
+    return fact;
+}
+void factorial(int a[], int n) {
+    for (int i = 0; i < n; ++i) {
+        a[i] = fact(a[i]);
+    }
+}
+
+int main()
+{
+    int n;
+    scanf_s("%i", &n);
+    int a[100];
+    for (int i = 0; i < n; ++i) {
+        scanf_s("%i", &a[i]);
+    }
+    factorial(a, n);
+    for (int i = 0; i < n; ++i) {
+        printf("%i ", a[i]);
+    }
+}
