4a944511dcc4abbe16062a7a507ab2e3ff3593a5
gr4m0zekka <101568879+gr4m0zekka@users.noreply.github.com>
2022-09-24 19:57:20 +0300

    Add files via upload

---
+++ 3_task/14_assign.c
@@ -0,0 +1,31 @@
+﻿
+
+#include <iostream>
+#define MAX 100
+
+void assign(int A[MAX][MAX], int B[MAX][MAX], int n) {
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            A[i][j] = B[i][j];
+        }
+    }
+}
+int main()
+{
+    int n;
+    scanf_s("%i", &n);
+    int B[MAX][MAX];
+    int A[MAX][MAX];
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            scanf_s("%i", &B[i][j]);
+        }
+    }
+    assign(A, B, n);
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            printf("%i", A[i][j]);
+        }
+    }
+}
+
