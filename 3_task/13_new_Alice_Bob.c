8a634b871d8706e2d7d156fda9ab9ce72d2c39e1
gr4m0zekka <101568879+gr4m0zekka@users.noreply.github.com>
2022-09-25 21:32:51 +0300

    Add files via upload

---
+++ 3_task/13_new_Alice_Bob.c
@@ -0,0 +1,28 @@
+﻿
+
+#include <iostream>
+void Bob(int n);
+void Alice(int n) {
+    n *= 3;
+    ++n;
+    printf("%i\n", n);
+    Bob(n);
+}
+void Bob(int n) {
+    while (n % 2 != 1) {
+            n /= 2;
+            printf("%i\n", n);
+        if (n == 1)
+        {
+            return ;
+        }
+    }
+        Alice(n);
+}
+int main()
+{
+    int n;
+    scanf_s("%i", &n);
+    Alice(n);
+
+}
