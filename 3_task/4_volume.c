b1eed5f20aafc0ff60f92b3f5ca66905bf6032b8
gr4m0zekka <101568879+gr4m0zekka@users.noreply.github.com>
2022-09-28 00:45:01 +0300

    Add files via upload

---
+++ 3_task/4_volume.c
@@ -0,0 +1,31 @@
+﻿
+
+#include <iostream>
+#include <math.h>
+int fact(int n) {
+    int fact = 1;
+    if (n) {
+        for (int i = 1; i <= n; ++i) {
+            fact *= i;
+        }
+    }
+    else {
+        return 1;
+    }
+    return fact;
+}
+float volume(int n) {
+    if (n % 2) {
+        return 2 * fact((n - 1) / 2) * pow(4 * 3.1415926, (n - 1) / 2) / fact(n);
+    }
+    else {
+        return pow(3.1415926, n / 2) / fact(n / 2);
+    }
+}
+int main()
+{
+    int n;
+    scanf_s("%i", &n);
+    printf("%f", pow(2, n) / volume(n));
+}
+
