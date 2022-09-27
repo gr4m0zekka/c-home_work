b87f466a860ea9c102b456b2ab48d42e76909d21
gr4m0zekka <101568879+gr4m0zekka@users.noreply.github.com>
2022-09-27 19:14:07 +0300

    Add files via upload

---
+++ 4_task/2_placements.c
@@ -0,0 +1,17 @@
+﻿
+#include <iostream>
+unsigned long long A_n_k(int n, int k)
+{
+	unsigned long long result = 1;
+	for (int i = n; i > n - k; --i)
+		result *= i;
+	return result;
+}
+
+int main()
+{
+	int n, k;
+	scanf_s("%i %i", &n, &k);
+	printf("%llu", A_n_k(n, k));
+}
+
---
+++ 4_task/3_yearfrac.c
@@ -0,0 +1,19 @@
+﻿
+
+#include <iostream>
+float yearfrac(int year, int day) {
+    float day_f = day;
+    if (year % 4) {
+        return day_f / 365;
+    }
+    else {
+        return day_f / 366;
+    }
+}
+int main()
+{
+    int year, day;
+    scanf_s("%i %i", &year, &day);
+    printf("%.5f", yearfrac(year, day));
+}
+
