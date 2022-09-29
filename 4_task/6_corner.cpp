4eba35c5a81238a49180781de5b8ce4b5bca198f
gr4m0zekka <101568879+gr4m0zekka@users.noreply.github.com>
2022-09-30 00:48:22 +0300

    Add files via upload

---
+++ 4_task/6_corner.cpp
@@ -0,0 +1,28 @@
+﻿
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+double distance(double x1, double y1, double x2, double y2) {
+    return sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+}
+double length(double x, double y) {
+    return distance(x, y, 0, 0);
+}
+double scalar_product(double x1, double y1, double x2, double y2) {
+    return x1 * x2 + y1 * y2;
+}
+const double pi = 3.14159265359;
+double to_degrees(double rad) {
+    return rad * 180 / pi;
+}
+
+int main()
+{
+    double  x, y;
+    double  x1, y1;
+    cin >> x >> y;
+    cin >>  x1 >> y1;
+    cout << to_degrees(acos(scalar_product(x, y, x1, y1) / (length(x,y)) * length(x1, y1)));
+}
+
