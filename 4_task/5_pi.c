1538fb041b4c9b60076a1094c20661912001daf8
gr4m0zekka <101568879+gr4m0zekka@users.noreply.github.com>
2022-09-29 18:02:32 +0300

    Add files via upload

---
+++ 4_task/5_pi.c
@@ -0,0 +1,18 @@
+﻿
+
+#include <iostream>
+#include <cmath>
+float pi(int n) {
+    float pi = 0;
+    for (int i = 1; i <= n; ++i) {
+        pi += 4 * pow(-1, i + 1) / (2 * i - 1);
+    }
+    return pi;
+}
+int main()
+{
+    int n;
+    scanf_s("%i", &n);
+    printf("%f", pi(n));
+}
+
