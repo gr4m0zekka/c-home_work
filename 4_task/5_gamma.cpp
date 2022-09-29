a2d20f8d5189503495eba6026e725dc885e19eda
gr4m0zekka <101568879+gr4m0zekka@users.noreply.github.com>
2022-09-30 00:21:48 +0300

    cpp, потому что scanf_s не работает с double

---
+++ 4_task/5_gamma.cpp
@@ -0,0 +1,23 @@
+﻿
+#include <iostream>
+#include <math.h>
+using namespace std;
+double gamma(double x) {
+    double integr = 0;
+    const double dt = 0.01;
+    const double eps = pow(10, -10);
+    double t = 0;
+    double sq = (pow(t, x - 1)) * exp(- t) + pow(t + dt, x - 1) * exp(- t - dt) / 2 * dt;
+    while (sq >= eps) {
+        integr += sq;
+        t += dt;
+        sq = (pow(t, x - 1) * exp(-t) + pow(t + dt, x - 1) * exp(-t - dt)) / 2 * dt;
+    }
+    return integr;
+}
+int main()
+{
+    double x;
+    cin >> x;
+    cout << gamma(x);
+}
