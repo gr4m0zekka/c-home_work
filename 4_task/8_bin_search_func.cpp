f46a9fa5e9f309f585c07681b32bc60e2ceaa58c
gr4m0zekka <101568879+gr4m0zekka@users.noreply.github.com>
2022-09-30 12:46:18 +0300

    Add files via upload

---
+++ 4_task/8_bin_search_func.cpp
@@ -0,0 +1,27 @@
+﻿
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+double f(double x) {
+    return pow(x, 5) + 2 * pow(x, 4) + 5 * pow(x, 2) + 4 * x - 500;
+}
+const double eps = pow(10, -10);
+
+int main()
+{
+    double l, h;
+    cin >> l >> h;
+    while (h - l > eps) {
+         double m = (l + h) / 2;
+         if (f(m) < 0) {
+             l = m;
+         }
+         else if (f(m) > 0) {
+             h = m;
+         }
+    }
+    cout << h;
+}
+
