b3e8595b12fa373868e60cdc5b95eb8098536fef
gr4m0zekka <101568879+gr4m0zekka@users.noreply.github.com>
2022-09-30 13:23:09 +0300

    Add files via upload

---
+++ 4_task/9_size_of_type.c
@@ -0,0 +1,28 @@
+﻿
+
+#include <iostream>
+
+int main()
+{
+    printf("char: %i bites", sizeof(char));
+    printf("\n");
+    printf("short: %i bites", sizeof(short));
+    printf("\n");
+    printf("int: %i bites", sizeof(int));
+    printf("\n");
+    printf("long long: %i bites", sizeof(long long));
+    printf("\n");
+    printf("size_t: %i bites", sizeof(size_t));
+    printf("\n");
+    printf("float: %i bites", sizeof(float));
+    printf("\n");
+    printf("double: %i bites", sizeof(double));
+    printf("\n");
+    printf("int*: %i bites", sizeof(int*));
+    printf("\n");
+    int a[10];
+    printf("int[10]: %i bites", sizeof(a));
+        
+    
+}
+
