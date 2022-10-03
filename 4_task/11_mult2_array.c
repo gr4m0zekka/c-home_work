bb57c082d0830b5914b53601456bdd05be8596ce
gr4m0zekka <101568879+gr4m0zekka@users.noreply.github.com>
2022-10-03 17:53:10 +0300

    Add files via upload

---
+++ 4_task/11_mult2_array.c
@@ -0,0 +1,23 @@
+﻿
+
+#include <iostream>
+void mult2_array(int* p, size_t n) {
+    for (int i = 0; i < n; ++i) {
+        *p *= 2;
+        ++p;
+    }
+}
+int main()
+{
+    int a[15];
+    size_t n = 10;
+    for (size_t i = 0; i < n; ++i) {
+        a[i] = i;
+    }
+    int* p = &a[0];
+    mult2_array(p, n);
+    for (size_t i = 0; i < n; ++i) {
+        printf("%i ", a[i]);
+    }
+}
+
---
+++ 4_task/12_solve_quadratic.cpp
@@ -0,0 +1,32 @@
+﻿
+
+#include <iostream>
+#include <math.h>
+const double eps = pow(10, -10);
+int solve_quadratic(double a, double b, double c, double* px1, double* px2) {
+    double d = pow(b, 2) - 4 * a * c;
+    if (d + eps < 0) {
+        return 0;
+    }
+    else if (d + eps > 0 && d - eps < 0) {
+        double sol = -b / (2 * a);
+        *px1 = sol;
+        return 1;
+    }
+    else if (d - eps > 0) {
+        *px1 = (-b - sqrt(d)) / (2 * a);
+        *px2 = (-b + sqrt(d)) / (2 * a);
+        return 2;
+    }
+}
+using namespace std;
+int main()
+{
+    double a, b, c, sl1, sl2;
+    cin >> a >> b >> c;
+    double* px1 = &sl1;
+    double* px2 = &sl2;
+    solve_quadratic(a, b, c, px1, px2);
+    cout << sl1 << " " << sl2;
+}
+
