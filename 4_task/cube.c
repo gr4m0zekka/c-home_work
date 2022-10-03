688dd8ffd07b6162e3eb2627fb370bb9afb6932b
gr4m0zekka <101568879+gr4m0zekka@users.noreply.github.com>
2022-10-03 16:30:00 +0300

    Add files via upload

---
+++ 4_task/cube.c
@@ -0,0 +1,15 @@
+﻿
+
+#include <iostream>
+#include <math.h>
+float cube(float* p) {
+    return pow(*p, 3);
+}
+int main()
+{
+    float a;
+    scanf_s("%f", &a);
+    float* p = &a;
+    printf("%f", cube(p));
+}
+
