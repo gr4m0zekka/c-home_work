c75c196effa179e60ef48a668240fe88846e4092
gr4m0zekka <101568879+gr4m0zekka@users.noreply.github.com>
2022-10-04 17:19:51 +0300

    Add files via upload

---
+++ 5_task/1_symb_type.c
@@ -0,0 +1,21 @@
+﻿
+
+#include <iostream>
+
+int main()
+{
+    char n;
+    scanf_s("%c", &n);
+    if ('a' <= n && n <= 'z') {
+        printf("Lowercase Letter");
+    }
+    else if ('A' <= n && n <= 'Z') {
+        printf("Uppercase Letter");
+    }
+    else if ('0' <= n && n <= '9') {
+        printf("Digit");
+    }
+    else {
+        printf("Other");
+    }
+}
