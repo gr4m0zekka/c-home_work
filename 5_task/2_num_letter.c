8d6f72fd0d77289e341a9fd8d11a050da6b32ae3
gr4m0zekka <101568879+gr4m0zekka@users.noreply.github.com>
2022-10-04 17:40:26 +0300

    Add files via upload

---
+++ 5_task/2_num_letter.c
@@ -0,0 +1,18 @@
+﻿
+
+#include <iostream>
+
+int main()
+{
+    char n;
+    scanf_s("%c", &n);
+    if ('a' <= n && n <= 'z') {
+        printf("%hhi", n - 'a' + 1);
+    }
+    else if ('A' <= n && n <= 'Z') {
+        printf("%hhi", n - 'A' + 1);
+    }
+    else {
+        printf("Not a letter");
+    }
+}
