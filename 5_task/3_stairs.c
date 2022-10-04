ddbadd16acfc7a49a2a10453f71e8693925ba0ca
gr4m0zekka <101568879+gr4m0zekka@users.noreply.github.com>
2022-10-04 18:16:00 +0300

    Add files via upload

---
+++ 5_task/3_stairs.c
@@ -0,0 +1,18 @@
+﻿
+
+#include <iostream>
+
+int main()
+{
+    char w[100];
+    scanf_s("%s", &w);
+    int i = 0;
+    while (w[i]) {
+        for (int k = 0; k <= i; ++k) {
+            printf("%c", w[k]);
+        }
+        printf("/n");
+        ++i;
+    }
+}
+
