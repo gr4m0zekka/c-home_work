9659e8254f1a14f09b44bdace3a3708add0b2ebe
gr4m0zekka <101568879+gr4m0zekka@users.noreply.github.com>
2022-10-07 00:37:03 +0300

    Add files via upload

---
+++ 5_task/6_bracket sequence.c
@@ -0,0 +1,26 @@
+﻿
+
+#include <stdio.h>
+int main()
+{
+	char str[50];
+	int calc = 0;
+	scanf("%s", str);
+	for (int i = 0; str[i]; ++i) {
+		if (str[i] == '(') {
+			++calc;
+		}
+		else {
+			--calc;
+		}
+		if (calc == -1) {
+			break;
+		}
+	}
+	if (calc) {
+		printf("No");
+	}
+	else {
+		printf("Yes");
+	}
+}
