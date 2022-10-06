fa224db03c0dbd14ddeffaabf6fb3f0152d2da15
gr4m0zekka <101568879+gr4m0zekka@users.noreply.github.com>
2022-10-06 23:39:01 +0300

    Add files via upload

---
+++ 5_task/5_vosklicanie.c
@@ -0,0 +1,22 @@
+﻿
+
+#include <iostream>
+
+
+
+int main()
+{
+    char text[50];
+    scanf("%[^\n]", text);
+    int i = 0;
+    while (text[i] != 0) {
+        if (text[i] == ' ' && isalpha(text[i - 1])) {
+            printf("! ");
+        }
+        else {
+            printf("%c", text[i]);
+        }
+        ++i;
+    }
+}
+
