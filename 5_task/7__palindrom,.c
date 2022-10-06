1c82027bc106c3244aa57488a9e232dacb0532aa
gr4m0zekka <101568879+gr4m0zekka@users.noreply.github.com>
2022-10-07 00:57:01 +0300

    Add files via upload

---
+++ 5_task/7__palindrom,.c
@@ -0,0 +1,30 @@
+﻿
+
+#include <stdio.h>
+int len_search(char a[]) {
+    int calc = 0;
+    for (int i = 0; a[i]; ++i) {
+        calc++;
+    }
+    return calc;
+}
+int is_palindrom(char a[]) {
+    for (int i = 0; a[i]; ++i) {
+        if (a[i] != a[len_search(a) - i]) {
+            return 0;
+        }
+    }
+    return 1;
+}
+int main()
+{
+    char str[50];
+    scanf("%s", str);
+    if (is_palindrom) {
+        printf("Yes");
+    }
+    else {
+        printf("No");
+    }
+}
+
