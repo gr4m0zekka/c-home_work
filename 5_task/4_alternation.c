54d7482fdc103207ac26102fa7df24171d0c343f
gr4m0zekka <101568879+gr4m0zekka@users.noreply.github.com>
2022-10-06 21:51:18 +0300

    Add files via upload

---
+++ 5_task/4_alternation.c
@@ -0,0 +1,29 @@
+﻿
+
+#include <iostream>
+
+int main()
+{
+    char w1[30];
+    char w2[20];
+    scanf("%s", w1);
+    scanf("%s", w2);
+    int i = 0;
+    while (w1[i] && w2[i]) {
+        printf("%c%c", w1[i], w2[i]);
+        ++i;
+    }
+    if (w1[i]) {
+        while (w1[i]) {
+            printf("%c", w1[i]);
+            ++i;
+        }
+    }
+    else if (w2[i]) {
+        while (w2[i]) {
+            printf("%c", w2[i]);
+            ++i;
+        }
+    }
+}
+
