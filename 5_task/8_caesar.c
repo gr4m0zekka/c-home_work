5f6106f6a95188a997089d70383408e0ea6d6426
gr4m0zekka <101568879+gr4m0zekka@users.noreply.github.com>
2022-10-07 01:58:45 +0300

    Add files via upload

---
+++ 5_task/8_caesar.c
@@ -0,0 +1,33 @@
+﻿ 
+
+#include <iostream>
+void encrypt(char* str, int k) {
+    for (int i = 0; str[i]; ++i) {
+        if ('a' <= str[i] && str[i] <= 'z') {
+            if (str[i] + k <= 'z') {
+                printf("%c", str[i] + k);
+            }
+            else {
+                printf("%c", 'a' + (str[i] + k) % 'z' - 1);
+            }
+        }
+        if ('A' <= str[i] && str[i] <= 'Z') {
+            if (str[i] + k <= 'Z') {
+                printf("%c", str[i] + k);
+            }
+            else {
+                printf("%c", 'A' + (str[i] + k) % 'Z' - 1);
+            }
+        }
+    }
+}
+int main()
+{
+    char str[50];
+    int k;
+    scanf("%i", &k);
+    scanf("%s", str);
+    encrypt(str, k);
+}
+
+
