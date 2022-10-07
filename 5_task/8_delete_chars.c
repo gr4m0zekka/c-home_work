d18203bb2bcdc83f4d1d198208b19aabda854c3f
gr4m0zekka <101568879+gr4m0zekka@users.noreply.github.com>
2022-10-07 22:42:19 +0300

    Add files via upload

---
+++ 5_task/8_delete_chars.c
@@ -0,0 +1,26 @@
+﻿
+
+#include <iostream>
+#include <stdio.h>
+void delete_chars(char* str, char c) {
+    for (int i = 0; str[i]; ++i) {
+        if (str[i] == c) {
+            for (int j = i; str[j]; ++j) {
+                str[j] = str[j + 1];
+            }
+        }
+    }
+}
+int main()
+{
+    char a[100];
+    char c;
+    scanf("%c", &c);
+    scanf("%s", a);
+    delete_chars(a, c);
+    for (int i = 0; a[i]; ++i) {
+        printf("%c", a[i]);
+    }
+
+}
+
