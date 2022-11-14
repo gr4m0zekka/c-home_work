1815b7ea766db83950f176a05d9e3ea4a2a9c74c
gr4m0zekka <101568879+gr4m0zekka@users.noreply.github.com>
2022-11-15 01:14:01 +0300

    Add files via upload

---
+++ 2_1_task/7_vector3f.cpp
@@ -0,0 +1,22 @@
+﻿
+#include "vector3f.h"
+#include <iostream>
+
+using std::cout;
+using std::endl;
+int main()
+{
+	vector3f a = { 1.0, 2.0, -2.0 };
+	vector3f b = { 4.0, -1.0, 3.0 };
+	cout << "a = " << a << endl << "b = " << b << endl;
+	cout << "a + b = " << a + b << endl;
+	cout << "-a = " << -a << endl;
+	cout << "Scalar product of a and b = " << a * b << endl;
+	a /= 5;
+	cout << "a after a /= 5;" << a << endl;
+		normalize(b);
+		cout << "Normalized b:" << b << endl;
+		a += b;
+	cout << "a after a+= b;" << a << endl;
+}
+
---
+++ 2_1_task/vector3f.h
@@ -0,0 +1,130 @@
+﻿#pragma once
+#include <cmath>
+#include <iostream>
+
+struct vector3f
+{
+    float x;
+    float y;
+    float z;
+};
+
+vector3f operator+ (const vector3f & a, const vector3f & b) /*сложение векторов*/
+{
+    vector3f result = { a.x + b.x, a.y + b.y, a.z + b.z };
+    return result;
+}
+
+vector3f operator-(const vector3f& a, const vector3f& b) /*вычитание векторов*/
+{
+    vector3f result = { a.x - b.x, a.y - b.y, a.z - b.z };
+    return result;
+}
+
+vector3f operator*(const vector3f& a, const float& b) /*умножение вектора на число*/
+{
+    vector3f result = { a.x * b, a.y * b, a.z * b };
+    return result;
+}
+
+vector3f operator*(const float& b, const vector3f& a) /*умножение числа на вектор*/
+{
+    vector3f result = { a.x * b, a.y * b, a.z * b };
+    return result;
+}
+
+float operator*(const vector3f& a, const vector3f& b) /*умножение векторов*/
+{
+    float result =  a.x * b.x + a.y * b.y + a.z * b.z ;
+    return result;
+}
+
+vector3f operator/(const vector3f& a, const float& b) /*деление вектора на число*/
+{
+    vector3f result = { a.x / b, a.y / b, a.z / b };
+    return result;
+}
+
+bool operator==(const vector3f& a, const vector3f b) { /*проверка на равенство */
+    return a.x == b.x && a.y == b.y && a.z == b.z;
+}
+
+bool operator!=(const vector3f& a, const vector3f& b) { /*проверка на нерваенство*/
+    return a.x != b.x || a.y != b.y || a.z != b.z;
+}
+vector3f operator+=(vector3f a, const vector3f b) { /*увеличиваем вектор на вектор*/
+    a.x += b.x;
+    a.y += b.y;
+    a.z += b.z;
+    return a;
+ }
+
+vector3f operator-=(vector3f a, const vector3f b) { /*уменьшаем вектор на вектор*/
+    a.x -= b.x;
+    a.y -= b.y;
+    a.z -= b.z;
+    return a;
+}
+
+vector3f& operator*=(vector3f a, const float b) { /*умеожаем вектор на число*/
+    a.x *= b;
+    a.y *= b;
+    a.z *= b;
+    return a;
+}
+
+vector3f& operator/=(vector3f a, const float b) { /*деление вектора на число*/
+    a.x /= b;
+    a.y /= b;
+    a.z /= b;
+    return a;
+}
+
+vector3f operator-(const vector3f& a) /*унарный минус*/
+{
+    vector3f result;
+    result.x = -a.x;
+    result.y = -a.y;
+    result.z = -a.z;
+    return result;
+}
+
+
+vector3f operator+(const vector3f& a) /*унарный плюс*/
+{
+    return a;
+}
+
+std::ostream& operator<<(std::ostream& out, const vector3f& a) { /*вывод вектора*/
+    out << '(' << a.x << "," << a.y << "," << a.z << ')';
+    return out;
+}
+
+std::istream& operator>>(std::istream& in, vector3f& c) { /*ввод вектора*/
+    in >> c.x >> c.y >> c.z;
+    return in;
+}
+
+float squaredNorm(const vector3f& a) { /*возвращает квадрат нормы*/
+    return a.x * a.x + a.y * a.y + a.z * a.z;
+}
+
+float norm(const vector3f& a) { /*возвращает норму */
+    return sqrt(a.x * a.x + a.y * a.y + a.z * a.z);
+}
+
+void normalize(vector3f& a) { /*нормализуем вектор а*/
+    a.x = a.x / norm(a);
+    a.y = a.y / norm(a);
+    a.z = a.z / norm(a);
+}
+
+
+
+
+
+
+
+
+
+
