828818e35133f5c35f3698b5332e48ee9bfc3b87
gr4m0zekka <101568879+gr4m0zekka@users.noreply.github.com>
2023-03-02 23:11:15 +0300

    Add files via upload

---
+++ 2_5/containers_2_[]{}().cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <stack>
+
+bool is_right(std::string& l) {
+    std::stack<char> st;
+    st.push(l[0]);
+    for (int i = 1; i < l.size(); i++) {
+        if (l[i] - 1 == st.top() || l[i] - 2 == st.top()) {
+            st.pop();
+        }
+        else {
+            st.push(l[i]);
+        }
+    }
+    return st.empty();
+}
+int main()
+{
+    std::string l =  "((({}])))";
+    if (is_right(l)) {
+        std::cout << "YES" << std::endl;
+    }
+    else {
+        std::cout << "NO" << std::endl;
+    }
+}
+
