8e8289214340fb426728af969cfb7865253155c3
gr4m0zekka <101568879+gr4m0zekka@users.noreply.github.com>
2023-05-05 10:00:46 +0300

    Add files via upload

---
+++ 2_6_task/algorithms_2.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+
+void shake(std::string& s) {
+    std::reverse(s.begin(), s.end());
+}
+
+void rev(std::vector<std::string>& str) {
+    std::reverse(str.begin(), str.end());
+    std::for_each(str.begin(), str.end(), shake);
+}
+
+int main()
+{
+    std::vector<std::string> str{ "cat", "dog", "mouse", "elephant" };
+    rev(str);
+    for (int i = 0; i < str.size(); i++) {
+        std::cout << str[i] << std::endl;
+    }
+}
+
