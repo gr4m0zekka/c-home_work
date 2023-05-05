8a57841c49ced86f2233ffda93666f39e6ddbc8a
gr4m0zekka <101568879+gr4m0zekka@users.noreply.github.com>
2023-05-05 10:27:37 +0300

    Add files via upload

---
+++ 2_6_task/algorithms_3.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <string_view>
+#include <algorithm>
+#include <cctype>
+
+bool chec_sym(std::string str) {
+    return std::all_of(str.begin(), str.end(), [](char c) {return !(c >= 97 && c <= 122); });
+}
+
+int main()
+{
+    std::string str{ "CATS AND Dogs12301@!" };
+    if (chec_sym(str)) {
+        std::cout << "True" << std::endl;
+    }
+    else {
+        std::cout << "False" << std::endl;
+    }
+}
+
