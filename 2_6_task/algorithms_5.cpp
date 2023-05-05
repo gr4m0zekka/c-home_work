118dca459d26dc46aceaefc5cbe981e25007a2fc
gr4m0zekka <101568879+gr4m0zekka@users.noreply.github.com>
2023-05-05 12:08:48 +0300

    Add files via upload

---
+++ 2_6_task/algorithms_5.cpp
@@ -0,0 +1,18 @@
+﻿
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <string_view>
+
+void move_probel(std::string& str) {
+    std::partition(str.begin(), str.end(), [](char c) {return c != 32; });
+}
+int main()
+{
+    std::string str{"gogs and cats"};
+    move_probel(str);
+    for (int i = 0; i < str.size(); i++)
+        std::cout << str[i] << std::endl;
+}
+
